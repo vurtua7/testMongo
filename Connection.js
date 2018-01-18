464d8fa59e17f728e497d9cef8a151cc7003042a
Andrew <vurtua7@hotmail.com>
2018-01-18 12:52:14 -0500

    testAgain

--- Connection.js
+++ Connection.js
@@ -4,6 +4,6 @@ var url = "mongodb://localhost:27017/";
 MongoClient.connect(url, (e,db) =>
 {
     if(e) throw e;
-    console.log("connected to mongodb");
+    console.log("connected to mongodb Hello World");
     db.close();
 })
